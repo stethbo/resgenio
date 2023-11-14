03b1d1e41ce9783152639b702b468b1aa6a171d4
stethbo <sztefan.borek@gmail.com>
2023-11-14 21:29:33 +0100

    adds log out and home buttons

--- app/routes.py
+++ app/routes.py
@@ -129,4 +129,4 @@ def download_resume(temp_file_path):
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('main.dashboard'))
